770c668488f0546c536e57af83fb57ca964f4fa9
ArnoldBF <abf1997.ab@gmail.com>
2024-01-22 10:36:41 -0400

    app node de clima terminada

--- app.js
+++ app.js
@@ -19,23 +19,39 @@ const main = async () => {
         const lugar = await leerInput("Ciudad: ");
         //buscar los lugares
         const lugares = await busquedas.ciudad(lugar);
+
         //seleccionar el lugar
         const id = await listarLugares(lugares);
-        const lugarSelec=lugares.find(l=>l.id===id);
-        console.log( {id });
-        console.log(lugarSelec);
+        if(id === '0') continue;
+        const lugarSelec = lugares.find((l) => l.id === id);
+       // console.log({ id });
+        //console.log(lugarSelec);
 
+        //Guardar en DB
+        busquedas.agregarHistorial(lugarSelec.nombre);
         //clima
-
+        const clima = await busquedas.climaLugar(
+          lugarSelec.lat,
+          lugarSelec.lng
+        );
         //mostrar resultado
+        console.clear();
         console.log("\nInformacion de la ciudad\n".green);
         console.log(`Ciudad: ${lugarSelec.nombre}`);
         console.log(`Lat: ${lugarSelec.lat}`);
         console.log(`Lng: ${lugarSelec.lng}`);
-        console.log("Temperatura:");
-        console.log("Minima:");
-        console.log("Maxima:");
+        console.log(`Temperatura: ${clima.temperatura}`);
+        console.log(`Minima: ${clima.minima}`);
+        console.log(`Maxima: ${clima.maxima}`);
+        console.log(`Descripcion: ${clima.descripcion}`);
 
+        break;
+        case 2:
+          
+         busquedas.historialRespetandoMayusc.forEach((lugar,i)=>{
+          const idx=`${i+1}.`.green;
+          console.log(`${idx} ${lugar}`);
+         })
         break;
     }
     if (opt !== 0) await pausa();
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,16 +1,37 @@
+const fs = require("fs");
+
 const axios = require("axios");
 
 class Busquedas {
-  historial = ["santacruz", "beni", "pando", "La paz"];
+  historial = [];
+  dbPath = "./DB/database.json";
 
   constructor() {
-    //leer DB si existee
+    this.leerDB();
+  }
+  get historialRespetandoMayusc() {
+    //return this.historials;
+    /*return this.historial.map((palabra) =>
+      palabra.replace(/\b\w/g, (l) => l.toUpperCase())
+    );**/
+    return this.historial.map(palabra=>{
+     let palabras=palabra.split(' ');
+     palabras=palabras.map(p=>p[0].toUpperCase()+p.substring(1));
+     return palabras.join(' ');
+    })
   }
   get paramsMapBox() {
     return {
-      limit: 5,
+      limit: 10,
       language: "es",
-      access_token:process.env.MAPBOX_KEY
+      access_token: process.env.MAPBOX_KEY,
+    };
+  }
+  get paramsOpenWeather() {
+    return {
+      appid: process.env.OPENWEATHER_KEY,
+      units: "metric",
+      lang: "es",
     };
   }
 
@@ -23,23 +44,61 @@ class Busquedas {
         params: this.paramsMapBox,
       });
       const resp = await instance.get();
-      return resp.data.features.map(lugar => ({
+      return resp.data.features.map((lugar) => ({
         id: lugar.id,
-        nombre:lugar.place_name,
-        lng:lugar.center[0],
-        lat:lugar.center[1],
+        nombre: lugar.place_name,
+        lng: lugar.center[0],
+        lat: lugar.center[1],
       }));
-     
     } catch (error) {
       return [];
     }
   }
-  async climaLugar(lat,lon){
-    try{
-    
-    }catch(error){
-     console.log(error);
+  async climaLugar(lat, lon) {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: { ...this.paramsOpenWeather, lat, lon },
+      });
+      const resp = await instance.get();
+
+      const { description } = resp.data.weather[0];
+      const { temp, temp_min, temp_max } = resp.data.main;
+      //  console.log(resp.data);
+
+      return {
+        descripcion: description,
+        temperatura: temp,
+        maxima: temp_max,
+        minima: temp_min,
+      };
+    } catch (error) {
+      console.log(error);
     }
+  }
+  agregarHistorial(lugar = "") {
+    //
+    if (this.historial.includes(lugar.toLocaleLowerCase())) {
+      return;
+    }
+    this.historial.unshift(lugar.toLocaleLowerCase());
+    //Grabar en DB
+    this.guardarDB();
+  }
+  guardarDB() {
+    const payload = {
+      historial: this.historial,
+    };
+
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+  leerDB() {
+    //debe de existir
+    if (!fs.existsSync(this.dbPath)) return null;
+    const info = fs.readFileSync(this.dbPath, { encoding: "utf-8" });
+    const data = JSON.parse(info);
+    //console.log(data);
+    this.historial = data.historial;
   }
 }
 
